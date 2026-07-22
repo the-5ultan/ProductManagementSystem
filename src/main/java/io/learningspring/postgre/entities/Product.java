6b5cf192fc2fc8ff109814de037c0dce8d592cf8
the-5ultan <rfgamers2005@gmail.com>
2026-07-22 23:45:30 +0500

    Reduced code with the lombok annotations !

--- src/main/java/io/learningspring/postgre/entities/Product.java
+++ src/main/java/io/learningspring/postgre/entities/Product.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Entity
+@Data                 //a lombok notation that autogenrates the getter and setters to reduce code
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,53 +27,53 @@ public class Product {
         this.warranty = warranty;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getPlace() {
-        return place;
-    }
-
-    public void setPlace(String place) {
-        this.place = place;
-    }
-
-    public int getWarranty() {
-        return warranty;
-    }
-
-    public void setWarranty(int warranty) {
-        this.warranty = warranty;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "name='" + name + '\'' +
-                ", type='" + type + '\'' +
-                ", place='" + place + '\'' +
-                ", warranty=" + warranty +
-                '}';
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+//
+//    public String getPlace() {
+//        return place;
+//    }
+//
+//    public void setPlace(String place) {
+//        this.place = place;
+//    }
+//
+//    public int getWarranty() {
+//        return warranty;
+//    }
+//
+//    public void setWarranty(int warranty) {
+//        this.warranty = warranty;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Product{" +
+//                "name='" + name + '\'' +
+//                ", type='" + type + '\'' +
+//                ", place='" + place + '\'' +
+//                ", warranty=" + warranty +
+//                '}';
+//    }
 }
